279ecb9de39103daa2e3154ffb24d825d92c7fbc
wjdtnghks123 <wjdtnghks123@nate.com>
2023-09-19 12:52:57 +0900

    TEST: kakao login

--- client/src/components/Login/Login.jsx
+++ client/src/components/Login/Login.jsx
@@ -8,6 +8,7 @@ export default function Login() {
   return (
     <div className='h-full w-full flex flex-col justify-center items-center'>
       <Logo />
+      <a id="kakao" href="/auth/kakao" class="btn">카카오톡 로그인</a>
       <section
         className="w-96 h-80 bg-transparent 
         flex flex-col justify-center items-center rounded-md border-2 border-myGray"
